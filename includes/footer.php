2c3fc8fba159197d37462625b7362129ade08eb2
Vinod Kumar <webindianlion@gmail.com>
2023-05-16 07:59:55 +0530

    images and font added
    images and font added

---
+++ includes/footer.php
@@ -0,0 +1,123 @@
+<!-- Main Footer -->
+<footer class="main-footer style-three">
+		<div class="pattern-layer-three" style="background-image: url(images/background/pattern-14.png)"></div>
+		<div class="pattern-layer-four" style="background-image: url(images/background/pattern-15.png)"></div>
+		<!--Waves end-->
+    	<div class="auto-container">
+        	<!--Widgets Section-->
+            <div class="widgets-section">
+            	<div class="row clearfix">
+                	
+					<!-- Footer Column -->
+					<div class="footer-column col-lg-5 col-md-6 col-sm-12">
+						<div class="footer-widget logo-widget">
+							<h5>About Vinccent Biotech Pvt. Ltd.</h5>
+							<div class="text">Vinccent Biotech Pvt. Ltd. is a diversified company from fair deal group of companies (FDG) who deals in automobile,pharmaceuticals,infrastructure development and having a group turnover of 170 Mn $.Vinccent Biotech Pvt. Ltd is well established pharmaceutical export company since 2014 based in the capital of India,Delhi (INDIA).</div>
+						</div>
+					</div>
+					
+					<!-- Footer Column -->
+					<div class="footer-column col-lg-4 col-md-6 col-sm-12">
+						<div class="footer-widget links-widget">
+							<h5>Products</h5>
+							<ul class="list-link">
+								<li><a href="anthelmentic.html">Anthelmentic</a></li>
+											<li><a href="antifungal.html">Antifungal</a></li>
+										<li><a href="antiinfectives.html">Antiinfectives</a></li>
+										<li><a href="antimalarial.html">Antimalarial</a></li>
+										<li><a href="cough-syrup.html">Cough Syrup</a></li>
+										<li><a href="antidiabetic.html">Antidiabetic</a></li>
+										
+							</ul>
+						</div>
+					</div>
+					
+                    <!-- Footer Column -->
+					<div class="footer-column col-lg-3 col-md-4 col-sm-12">
+								<div class="footer-widget contact-widget">
+									<h5>Contact Us</h5>
+									<ul>
+										<li>
+											<span class="icon flaticon-placeholder-2"></span>
+											<strong>Address</strong>
+											Plot no 4-6, 1st floor Main G.T.Road, Dilshad garden Delhi-110095, (India)
+										</li>
+										<li>
+											<span class="icon flaticon-phone-call"></span>
+											<strong>Phone</strong>
+											<a href="tel:+786-875-864-75">+91 8800475284</a>
+										</li>
+										<li>
+											<span class="icon flaticon-email-1"></span>
+											<strong>E-Mail</strong>
+											<a href="mailto:support@globex.com">info@vinccent.in</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+					
+				</div>
+			</div>
+			
+		</div>
+		
+		<!-- Footer Bottom -->
+		<div class="footer-bottom">
+			<div class="auto-container">
+				<div class="row clearfix">
+					<!-- Column -->
+					<div class="column col-lg-6 col-md-12 col-sm-12">
+						<div class="copyright">Copyright &copy; 2020 by Vinccent Biotech Pvt. Ltd.. All Rights Reserved.</div>
+					</div>
+					<!-- Column -->
+					
+				</div>
+			</div>
+		</div>
+		
+	</footer>
+	
+	
+</div>
+<!--End pagewrapper-->
+
+<!-- Color Palate / Color Switcher -->
+
+<!-- Search Popup -->
+<div class="search-popup">
+	<button class="close-search style-two"><span class="flaticon-multiply"></span></button>
+	<button class="close-search"><span class="flaticon-up-arrow-1"></span></button>
+	<form method="post" action="blog.html">
+		<div class="form-group">
+			<input type="search" name="search-field" value="" placeholder="Search Here" required="">
+			<button type="submit"><i class="fa fa-search"></i></button>
+		</div>
+	</form>
+</div>
+<!-- End Header Search -->
+
+<!--Scroll to top-->
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-arrow-up"></span></div>
+
+<script src="js/jquery.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="js/jquery.fancybox.js"></script>
+<script src="js/appear.js"></script>
+<script src="js/parallax.min.js"></script>
+<script src="js/tilt.jquery.min.js"></script>
+<script src="js/jquery.paroller.min.js"></script>
+<script src="js/owl.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/nav-tool.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/script.js"></script>
+<script src="js/color-settings.js"></script>
+
+
+
+
+
+</body>
+</html>
---
+++ includes/top-nav.php
@@ -0,0 +1,216 @@
+<!-- Preloader -->
+<div class="preloader"></div>
+ 	
+ 	<!-- Main Header-->
+    <header class="main-header header-style-one">
+    	
+		<!-- Header Top -->
+        <div class="header-top">
+            <div class="auto-container">
+                <div class="clearfix">
+					<!-- Top Left -->
+					<div class="top-left">
+						<!-- Info List -->
+						<ul class="info-list">
+							<li><a href="mailto:info@vinccent.in"><span class="icon flaticon-email"></span> info@vinccent.in</a></li>
+							<li><a href="tel:+91 8800475284"><span class="icon flaticon-telephone"></span> +91 8800475284</a></li>
+						</ul>
+					</div>
+					
+					<!-- Top Right -->
+                    <div class="top-right pull-right">
+
+
+                    	<div id="google_translate_element" style="display: inline-block;"></div>
+
+<script type="text/javascript" >
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+						<!-- Social Box -->
+						<ul class="social-box" style="display: inline-block;">
+							<li><a href="https://www.facebook.com/Vinccent-Biotech-PvtLtd-104464355015639" class="fa fa-facebook-f" target="_blank"></a></li>
+							<li><a href="#" class="fa fa-twitter"></a></li>
+							<li><a href="#" class="fa fa-dribbble"></a></li>
+							<li><a href="#" class="fa fa-google"></a></li>
+						</ul>
+                    </div>
+
+                    
+
+					
+                </div>
+            </div>
+        </div>
+		
+		<!--Header-Upper-->
+        <div class="header-upper">
+        	<div class="auto-container clearfix">
+            	
+				<div class="pull-left logo-box">
+					<div class="logo"><a href="index.html"><img src="images/logo.png" alt="" title=""></a></div>
+				</div>
+				
+				<div class="nav-outer clearfix">
+					<!--Mobile Navigation Toggler-->
+					<div class="mobile-nav-toggler"><span class="icon flaticon-menu"></span></div>
+					<!-- Main Menu -->
+					<nav class="main-menu navbar-expand-md">
+						<div class="navbar-header">
+							<!-- Toggle Button -->    	
+							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+						
+						<div class="navbar-collapse collapse clearfix" id="navbarSupportedContent">
+							<ul class="navigation clearfix">
+								<li ><a href="index.html">Home</a></li>
+								<li ><a href="aboutus.html">About Us</a></li>
+									
+								
+								<li class="dropdown"><a href="#">Products</a>
+									<ul>
+										<li><a href="anthelmentic.html">Anthelmentic</a></li>
+											<li><a href="antifungal.html">Antifungal</a></li>
+										<li><a href="antiinfectives.html">antiinfectives</a></li>
+										<li><a href="antimalarial.html">Antimalarial</a></li>
+										<li><a href="cough-syrup.html">Cough Syrup</a></li>
+										<li><a href="antidiabetic.html">Antidiabetic</a></li>
+										<li><a href="antihypertensives.html">AntiHypertensives</a></li>
+											<li><a href="antimicrobial.html">antimicrobial</a></li>
+											
+											<li><a href="gastro-intestinal.html">Gastro Intestinal</a></li>
+
+
+
+										
+
+									</ul>
+								</li>
+
+
+								<li class="dropdown"><a href="#">Our Team </a>
+									<ul>
+										<li><a href="burking-faso.html">Burkina Faso</a></li>
+											<li><a href="mali.html">Mali</a></li>
+										<li><a href="benin.html"> Benin</a></li>
+										<li><a href="togo.html">Togo</a></li>
+										
+
+
+										
+
+									</ul>
+								</li>
+
+
+
+								<li class="dropdown"><a href="#">Business Segment</a>
+									<ul>
+										<li><a href="branded-generics-business.html">Branded Generics Business</a></li>
+										<li><a href="medical-devices-business.html">Medical Devices Business</a></li>
+										
+
+									</ul>
+								</li>
+								
+								
+								
+								
+								<li><a href="contact.html">Contact us</a></li>
+							</ul>
+
+						</div>
+					</nav>
+
+
+					
+					<!-- Main Menu End-->
+					<div class="outer-box clearfix">
+					
+						<!-- Cart Box -->
+						<div class="cart-box">
+							<div class="dropdown">
+								
+								<div class="dropdown-menu pull-right cart-panel" aria-labelledby="dropdownMenu1">
+
+									<div class="cart-product">
+										<div class="inner">
+											<div class="cross-icon"><span class="icon fa fa-remove"></span></div>
+											<div class="image"><img src="images/resource/post-thumb-1.jpg" alt="" /></div>
+											<h3><a href="shop-single.html">Flying Ninja</a></h3>
+											<div class="quantity-text">Quantity 1</div>
+											<div class="price">$99.00</div>
+										</div>
+									</div>
+									<div class="cart-product">
+										<div class="inner">
+											<div class="cross-icon"><span class="icon fa fa-remove"></span></div>
+											<div class="image"><img src="images/resource/post-thumb-2.jpg" alt="" /></div>
+											<h3><a href="shop-single.html">Patient Ninja</a></h3>
+											<div class="quantity-text">Quantity 1</div>
+											<div class="price">$99.00</div>
+										</div>
+									</div>
+									<div class="cart-total">Sub Total: <span>$198</span></div>
+									<ul class="btns-boxed">
+										<li><a href="shoping-cart.html">View Cart</a></li>
+										<li><a href="checkout.html">CheckOut</a></li>
+									</ul>
+
+								</div>
+							</div>
+						</div>
+						
+						<!-- Search Btn -->
+						
+						<!-- Nav Btn -->
+						
+						
+						
+					</div>
+				</div>
+				
+            </div>
+        </div>
+        <!--End Header Upper-->
+        
+		<!-- Sticky Header  -->
+        <div class="sticky-header">
+            <div class="auto-container clearfix">
+                <!--Logo-->
+                <div class="logo pull-left">
+                    <a href="index.html" title=""><img src="images/logo-small.png" alt="" title=""></a>
+                </div>
+                <!--Right Col-->
+                <div class="pull-right">
+                    <!-- Main Menu -->
+                    <nav class="main-menu">
+                        <!--Keep This Empty / Menu will come through Javascript-->
+                    </nav><!-- Main Menu End-->
+					
+					
+					
+                </div>
+            </div>
+        </div><!-- End Sticky Menu -->
+    
+		<!-- Mobile Menu  -->
+        <div class="mobile-menu">
+            <div class="menu-backdrop"></div>
+            <div class="close-btn"><span class="icon flaticon-multiply"></span></div>
+            
+            <nav class="menu-box">
+                <div class="nav-logo"><a href="index.html"><img src="images/logo.png" alt="" title=""></a></div>
+                <div class="menu-outer"><!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header--></div>
+            </nav>
+        </div><!-- End Mobile Menu -->
+	
+    </header>
+    <!-- End Main Header -->
